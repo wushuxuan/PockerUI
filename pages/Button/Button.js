908d0422ced88013824d9352ca655a2b9f33d514
闲小璇 <30644407+wushuxuan@users.noreply.github.com>
2017-10-06 12:28:15 +0800

    add Button
    增加button部分

---
+++ pages/Button/Button.js
@@ -0,0 +1,83 @@
+// pages/Button/Button.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  getPhoneNumber:function(e){
+    console.log(e.detail.errMsg)
+    console.log(e.detail.iv)
+    console.log(e.detail.encryptedData)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return{
+      title:'PockerUI-转发分享',
+      path:'../index/index',
+      imageUrl:'http://scimg.jb51.net/allimg/160604/77-160604095Q1Y7.jpg',
+      success:function(res){
+        wx.showToast({
+          title: '转发成功',
+          icon:'success',
+          duration:1500
+        })
+      }
+      
+    }
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,88 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    RightArrow:'../../images/RightArrow.png',
+    indexList: [{
+      name:'Button 按钮',
+      url:'../Button/Button'
+    },{
+        name: 'Badege 徽章',
+        url: '../Badege/Badege'
+      },{
+        name: 'Capsule 胶囊',
+        url: '../Capsule/Capsule'
+      },{
+        name: 'Card 卡片',
+        url: '../Card/Card'
+      },{
+        name: 'Cell 单元格',
+        url: '../Cell/Cell'
+      },{
+        name: 'Dialog 弹出框',
+        url: '../Dialog/Dialog'
+      },{
+        name: 'Form 表单',
+        url: '../Form/Form'
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
