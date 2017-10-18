74a99ec3ae79bc9790485d97150e61bd709614f0
闲小ç? <30644407+wushuxuan@users.noreply.github.com>
2017-10-18 18:23:39 +0800

    add Map
    add Map

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,31 +6,35 @@ Page({
   data: {
     show:false,
     RightArrow:'../../images/RightArrow.png',
-    indexList: [{
+    indexList: [
+      {
+        name: 'Card 卡片',
+        url: '../Card/Card'
+      },
+      {
+        name: 'Map 地图',
+        url: '../Map/Map'
+      }, {
+        name: 'Form 表单',
+        url: '../Form/Form'
+      },{
       name:'Button 按钮',
       url:'../Button/Button'
-    },{
+      },{
         name: 'Badege 徽章',
         url: '../Badege/Badege'
       },{
         name: 'Capsule 胶囊',
         url: '../Capsule/Capsule'
-    },
-      {
-        name: 'Form 表单',
-        url: '../Form/Form'
+      }, {
+        name: 'Image 上传图片',
+        url: '../Image/Image'
       },{
       name: 'Progress 进度条',
       url: '../progress/progress'
-    },{
-        name: 'Card 卡片',
-        url: '../Card/Card'
       },{
         name: 'Picker 自定义选择器',
         url: '../Picker/Picker'
-      },{
-        name: 'Image 上传图片',
-        url: '../Image/Image'
       },
       {
         name: 'SearchMore 查看更多',
