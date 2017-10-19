284d268aab5b389120efb11b3ce23d771147b2c8
闲小ç? <30644407+wushuxuan@users.noreply.github.com>
2017-10-19 15:36:44 +0800

    add Audio's progress
    自定义带进度条的音乐播放控件

--- pages/Music/Music.js
+++ pages/Music/Music.js
@@ -5,62 +5,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
-  },
+    MusicList:[{
+      url: 'https://raw.githubusercontent.com/wushuxuan/PockerUI/master/Music/Music.mp3',
+      image:'https://y.gtimg.cn/music/photo_new/T002R300x300M000002ZXwUj3RlnVN.jpg?max_age=2592000',
+      name:'勋章',
+      author:'鹿晗',
+    }],
+    progress:0
+  }, 
+  MusicStart:function(e){
+    var progress = parseInt((e.detail.currentTime / e.detail.duration) * 100)
+    var that = this
+    that.setData({
+      progress: progress
+    })
+    console.log(progress+'%')
 
+  },
+  MusicEnd:function(){
+    wx.showModal({
+      title: '提示',
+      content: '音乐已播放完毕',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+   
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
