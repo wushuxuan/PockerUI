c9849dd258a2059c81dcfef77f3d1c57c1575bdc
wushuxuan <30644407+wushuxuan@users.noreply.github.com>
2017-11-09 17:11:17 +0800

    add 多选picker
    add 多选picker

--- pages/Picker/Picker.js
+++ pages/Picker/Picker.js
@@ -7,38 +7,73 @@ Page({
     showDialog: false,
     showDialogRight:false,
     items: [
-      { name: '中国', value: '中国' },
-      { name: '美国', value: '美国' },
-      { name: '巴西', value: '巴西' },
-      { name: '日本', value: '日本' },
-      { name: '英国', value: '英国' },
-      { name: '法国', value: '法国' },
-      { name: '韩国', value: '韩国' },
-      { name: '俄罗斯', value: '俄罗斯' },]
-
+      { name: '0', value: '中国', checked: false,},
+      { name: '1', value: '美国', checked: false,},
+      { name: '2', value: '巴西', checked: false,},
+      { name: '3', value: '日本', checked: false,},
+      { name: '4', value: '英国', checked: false,},
+      { name: '5', value: '法国', checked: false,},
+      { name: '6', value: '韩国', checked: false,},
+      { name: '7', value: '俄罗斯', checked: false,},],
   },
-  /*点击变色*/
   click:function(e){
+    console.log(e)
     var id = e.currentTarget.dataset.id
     var that = this
     that.setData({
-      id:id
-    })
-  },
-  onLoad: function (options) {
-    var that = this
-    that.setData({
-      value:'show'
+      id: id
     })
   },
   radioChange: function (e) {
-    console.log('radio发生change事件，携带value值为：', e.detail.value)
+    console.log('radio发生change事件，携带value值为：', e)
     var that = this
     that.setData({
       value: e.detail.value
     })
     console.log(this.data.value)
+    var items = this.data.items;
+    console.log(this.data.items)
+    var checkArr = e.detail.value;
+    console.log(e.detail.value)
+    for (var i = 0; i < items.length; i++) {
+      if (checkArr.indexOf(i + "") != -1) {
+        items[i].checked = true;
+      } else {
+        items[i].checked = false;
+      }
+    }
+    this.setData({
+      items: items
+    })  
   },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   /**普通picker */
   toggleDialog() {
     this.setData({
--- pages/WaterfallFlow/WaterfallFlow.js
+++ pages/WaterfallFlow/WaterfallFlow.js
@@ -5,94 +5,94 @@ Page({
    * 页面的初始数据
    */
   data: {
-    images:[
-      { 
-        id:'1',
-        src:'../../images/pubuliu/01.jpg',
-        name:'照片01',
-        data:'2017/11/1'
+    images: [
+      {
+        id: '1',
+        src: '../../images/pubuliu/01.jpg',
+        name: '照片01',
+        data: '2017/11/1'
       }, {
         id: '2',
         src: '../../images/pubuliu/02.jpg',
-        name: '照片01',
+        name: '照片02',
         data: '2017/11/2'
       }, {
         id: '3',
         src: '../../images/pubuliu/03.jpg',
-        name: '照片01',
+        name: '照片03',
         data: '2017/11/3'
       }, {
         id: '4',
         src: '../../images/pubuliu/04.jpg',
-        name: '照片01',
+        name: '照片04',
         data: '2017/11/4'
       }, {
         id: '5',
         src: '../../images/pubuliu/05.jpg',
-        name: '照片01',
+        name: '照片05',
         data: '2017/11/5'
       }, {
         id: '6',
         src: '../../images/pubuliu/06.jpg',
-        name: '照片01',
+        name: '照片06',
         data: '2017/11/6'
       }, {
         id: '7',
         src: '../../images/pubuliu/07.jpg',
-        name: '照片01',
+        name: '照片07',
         data: '2017/11/7'
       }, {
         id: '8',
         src: '../../images/pubuliu/08.jpg',
-        name: '照片01',
+        name: '照片08',
         data: '2017/11/8'
       }, {
         id: '9',
         src: '../../images/pubuliu/07.jpg',
-        name: '照片01',
+        name: '照片09',
         data: '2017/11/9'
       }, {
         id: '10',
         src: '../../images/pubuliu/08.jpg',
-        name: '照片01',
+        name: '照片10',
         data: '2017/11/10'
       }, {
         id: '11',
         src: '../../images/pubuliu/04.jpg',
-        name: '照片01',
+        name: '照片11',
         data: '2017/11/11'
       }, {
         id: '12',
         src: '../../images/pubuliu/05.jpg',
-        name: '照片01',
+        name: '照片12',
         data: '2017/11/12'
       }, {
         id: '13',
         src: '../../images/pubuliu/06.jpg',
-        name: '照片01',
+        name: '照片13',
         data: '2017/11/13'
       }, {
         id: '14',
         src: '../../images/pubuliu/07.jpg',
-        name: '照片01',
+        name: '照片14',
         data: '2017/11/14'
       }, {
         id: '15',
         src: '../../images/pubuliu/05.jpg',
-        name: '照片01',
+        name: '照片15',
         data: '2017/11/15'
       }, {
         id: '16',
         src: '../../images/pubuliu/07.jpg',
-        name: '照片01',
+        name: '照片16',
         data: '2017/11/16'
       }, {
         id: '17',
         src: '../../images/pubuliu/06.jpg',
-        name: '照片01',
+        name: '照片17',
         data: '2017/11/17'
       },
-      
+
     ]
   },
   ImageLoad:function(e){
